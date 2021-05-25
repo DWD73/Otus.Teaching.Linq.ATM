049a1d6fb62334c710d2aefd2fe8279da678409b
DDW <demin-dw@yandex.ru>
2021-05-26 00:14:11 +0300

    Добавьте файлы проекта.

---
+++ Otus.Teaching.Linq.ATM.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Otus.Teaching.Linq.ATM.Core.Services;
+using Otus.Teaching.Linq.ATM.DataAccess;
+
+namespace Otus.Teaching.Linq.ATM.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Старт приложения-банкомата...");
+
+            var atmManager = CreateATMManager();
+
+            //TODO: Далее выводим результаты разработанных LINQ запросов
+
+            foreach(var t in atmManager.GG("", "").ToList())
+                System.Console.WriteLine($" {t.FirstName}");
+
+
+
+            while(true)
+            {
+                
+            }
+
+
+            System.Console.WriteLine("Завершение работы приложения-банкомата...");
+        }
+
+        static ATMManager CreateATMManager()
+        {
+            using var dataContext = new ATMDataContext();
+            var users = dataContext.Users.ToList();
+            var accounts = dataContext.Accounts.ToList();
+            var history = dataContext.History.ToList();
+                
+            return new ATMManager(accounts, users, history);
+        }
+    }
+}
---
+++ Otus.Teaching.Linq.ATM.Core/Services/ATMManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Otus.Teaching.Linq.ATM.Core.Entities;
+
+namespace Otus.Teaching.Linq.ATM.Core.Services
+{
+    public class ATMManager
+    {
+        public IEnumerable<Account> Accounts { get; private set; }
+        
+        public IEnumerable<User> Users { get; private set; }
+        
+        public IEnumerable<OperationsHistory> History { get; private set; }
+        
+        public ATMManager(IEnumerable<Account> accounts, IEnumerable<User> users, IEnumerable<OperationsHistory> history)
+        {
+            Accounts = accounts;
+            Users = users;
+            History = history;
+        }
+
+        //TODO: Добавить методы получения данных для банкомата
+
+        public IEnumerable<User> GetUserInfo(string login, string password)
+        {           
+            return Users.Where(x => x.Login == login && x.Password == password);           
+        }
+    }
+}
