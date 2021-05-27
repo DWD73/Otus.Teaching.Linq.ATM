25ec4ffd861681bae47575ab07b60572ad45cb59
Дмитрий <User@DEMIN-DV>
2021-05-27 17:58:27 +0300

    Выполнены все задания

--- Otus.Teaching.Linq.ATM.Console/Program.cs
+++ Otus.Teaching.Linq.ATM.Console/Program.cs
@@ -6,24 +6,45 @@ using Otus.Teaching.Linq.ATM.DataAccess;
 
 namespace Otus.Teaching.Linq.ATM.Console
 {
+    using System;
+
     class Program
     {
-        
+
+        private static int UserId { get; set; }
 
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Старт приложения-банкомата...");
+            System.Console.WriteLine($"Старт приложения-банкомата...\n");
+
+            UserId = 0;
 
             var atmManager = CreateATMManager();
-            
+
 
             //TODO: Далее выводим результаты разработанных LINQ запросов
+            HelpMessage();
 
             ServiceManager(atmManager);
 
-            System.Console.WriteLine("Завершение работы приложения-банкомата...");
+            Console.WriteLine("Завершение работы приложения-банкомата...");
             
             
+        }
+
+        private static void HelpMessage()
+        {
+            Console.WriteLine($"Навигация\n");
+            Console.WriteLine($"\t{Command.UA} - Вывод информации клиента по логину и паролю");
+            Console.WriteLine($"\t{Command.UAC} - Вывод информации о счетах клиента");
+            Console.WriteLine($"\t{Command.UAH} - Вывод информации о счетах клиента и их историю");
+            Console.WriteLine($"\t{Command.UAll} - Вывод данных о всех операциях пополенения счёта " +
+                $"с указанием владельца каждого счёта");
+            Console.WriteLine($"\t{Command.UN} - Вывод данных о всех пользователях у которых на счёте " +
+                $"сумма больше N (N задаётся из вне и может быть любой);");
+            Console.WriteLine($"\t{Command.Help} - Помощь по навигации");
+
+            Console.WriteLine($"\t{Command.Exit} - Завершение программы\n");
         }
 
         static void ServiceManager(ATMManager aTMManager)
@@ -38,8 +59,17 @@ namespace Otus.Teaching.Linq.ATM.Console
                     case Command.UA:
                         UserInfo(aTMManager);
                         break;
-                    case Command.UACash:
+                    case Command.UAC:
                         UserInfoCash(aTMManager);
+                        break;
+                    case Command.UAH:
+                        UserInfoCashDetail(aTMManager);
+                        break;
+                    case Command.UAll:
+                        AllInputCashByUser(aTMManager);
+                        break;
+                    case Command.UN:
+                        AllAccountByCashSum(aTMManager);
                         break;
                     case Command.Exit:
                         Environment.Exit(0);
@@ -48,72 +78,164 @@ namespace Otus.Teaching.Linq.ATM.Console
             }
         }
 
-        private static int UserId { get; set; }
-
-        private static void UserInfo(ATMManager aTMManager)
-        {
-            string login = GetPasswordLogin("логин");
-            string password = GetPasswordLogin("пароль");
-
-            foreach (var t in aTMManager.GetUserInfo(login, password).ToList())
-            {
-                UserId = t.Id;
-                System.Console.WriteLine($"{t.ToString()}");
-            }
-                
-
-            System.Console.WriteLine();
-        }
-
-        private static void UserPrint()
-        {
-
-        }
-
         
 
-        private static void UserInfoCash(ATMManager aTMManager)
-        {
-            if (UserId <= 0)
-            {
-                System.Console.WriteLine("Нуль значение");
-            }
-            else
-            {
-                List<string> list = aTMManager.GetUserInfoCash(UserId);
-
-                for (int i = 0; i < aTMManager.GetUserInfoCash(UserId).Count; i++)
-                {
-                    System.Console.WriteLine(list[i]);
-                }
-                
-            }
-
-        
-            System.Console.WriteLine();
-        }
-
-        
+        #region  1. Вывод информации аккаунта по логину и паролю
 
         private static string GetPasswordLogin(string text)
         {
-            System.Console.WriteLine($"Введите {text}");
-            
-            while(true)
+            Console.WriteLine($"Введите {text}");
+
+            while (true)
             {
-                var input = System.Console.ReadLine();
-                if(!string.IsNullOrEmpty(input))
+                var input = Console.ReadLine();
+                if (!string.IsNullOrEmpty(input))
                 {
                     return input;
                 }
                 else
                 {
-                    System.Console.WriteLine($"{text} не должен быть пустым");
+                    Console.WriteLine($"{text} не должен быть пустым");
                 }
             }
+
+        }
+
+
+        /// <summary>
+        /// Вывод информации аккаунта по логину и паролю
+        /// </summary>
+        /// <param name="aTMManager"></param>
+        private static void UserInfo(ATMManager aTMManager)
+        {
+            string login = GetPasswordLogin("логин");
+            string password = GetPasswordLogin("пароль");
+
+            foreach (var item in aTMManager.GetUserInfo(login, password).ToList())
+            {
+                UserId = item.Id;
+                Console.WriteLine($"{item.ToString()}");
+            }
+                
+
+            Console.WriteLine();
+        }
+
+        #endregion
+
+        #region 2. Вывод данных о всех счетах заданного пользователя
+
+        /// <summary>
+        /// Вывод данных о всех счетах заданного пользователя
+        /// </summary>
+        /// <param name="aTMManager"></param>
+        private static void UserInfoCash(ATMManager aTMManager)
+        {
+            if (UserId <= 0)
+            {
+                Console.WriteLine("Не выбран клиент счета(ов)");
+            }
+            else
+            {
+                List<string> list = aTMManager.GetUserInfoCash(UserId);
+
+                Console.WriteLine($"Вывод информации о счетах клиента\n");
+
+                for (int i = 0; i < aTMManager.GetUserInfoCash(UserId).Count; i++)
+                {
+                    Console.WriteLine($"\t{list[i]}");
+                }
+                
+            }
+
+        
+            Console.WriteLine();
+        }
+
+
+        #endregion
+
+        #region 3. Вывод информации о счетах клиента и их историю
+
+        private static void UserInfoCashDetail(ATMManager aTMManager)
+        {
+            if (UserId <= 0)
+            {
+                Console.WriteLine("Не указан пользователь");
+            }
+            else
+            {
+                Console.WriteLine($"Вывод информации о счетах клиента и их историю\n");
+
+                foreach (var account in aTMManager.GetUserAccounts(UserId))
+                {
+                    PrintInfoCash(aTMManager, account);
+                    PrintInfoCashDetail(aTMManager, account);
+                }
+
+            }
+
+
+            Console.WriteLine();
+        }
+
+        private static void PrintInfoCash(ATMManager aTMManager, int _account)
+        {
+            foreach (var account in aTMManager.GetUserInfoCashByAccount(_account))
+                Console.WriteLine(account);
+        }
+
+        private static void PrintInfoCashDetail(ATMManager aTMManager, int _account)
+        {
+            foreach (var account in aTMManager.GetUserInfoCashByAccountDetail(_account))
+                Console.WriteLine($"{account}\n");
+        }
+
+
+        #endregion
+
+        #region 4. Вывод данных о всех операциях пополенения счёта с указанием владельца каждого счёта
+
+        private static void AllInputCashByUser(ATMManager aTMManager)
+        {
+            UserId = 0;
+
+            Console.WriteLine($"Вывод данных о всех операциях пополенения счёта\n");
+
+            foreach (var accountCash in aTMManager.GetAllInputCashByUser())
+                Console.WriteLine($"\t{accountCash}");
+        }
+
+        #endregion
+
+        #region 5. Вывод данных о всех пользователях у которых на счёте сумма больше N
+
+        private static void AllAccountByCashSum(ATMManager aTMManager)
+        {
+            UserId = 0;
+
+            decimal number;
+
+            Console.WriteLine($"Укажите сумму N для отбора\n");
+
+            var sum = Console.ReadLine();
+
+            if(!string.IsNullOrEmpty(sum) && decimal.TryParse(sum, out number))
+            {
+                Console.WriteLine($"Вывод данных о всех пользователях у которых на счёте сумма больше N\n");
+
+                foreach (var accountCash in aTMManager.GetAllAccountByCashSum(number))
+                    Console.WriteLine($"\t{accountCash}");
+            }
+            else
+            {
+                Console.WriteLine("Введите корректную сумму");
+            }
+
             
         }
 
+        #endregion
 
         private static Command ReadCommand()
         {
@@ -140,21 +262,14 @@ namespace Otus.Teaching.Linq.ATM.Console
                 }
                 else
                 {
-                    System.Console.WriteLine("Не должно быть пустым");
+                    System.Console.WriteLine("Комманда не должна иметь пустую строку");
                 }
             }
         }
 
        
 
-        private static void HelpMessage()
-        {
-            System.Console.WriteLine($"{Command.UA} - Вывод информации о заданном аккаунте по логину и паролю");
-            System.Console.WriteLine($"{Command.UACash} - Вывод информации о счетах заданного клиента");
-            System.Console.WriteLine($"{Command.Help} - Помощь по навигации");
-            System.Console.WriteLine($"{Command.Exit} - Завершение программы");
-        }
-
+       
         
 
         static ATMManager CreateATMManager()
@@ -174,7 +289,10 @@ namespace Otus.Teaching.Linq.ATM.Console
     {
         Help,
         UA,
-        UACash,
+        UAC,
+        UAH,
+        UAll,
+        UN,
         Exit
     }
 }
--- Otus.Teaching.Linq.ATM.Core/Services/ATMManager.cs
+++ Otus.Teaching.Linq.ATM.Core/Services/ATMManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Otus.Teaching.Linq.ATM.Core.Entities;
 
@@ -51,7 +52,32 @@ namespace Otus.Teaching.Linq.ATM.Core.Services
 
         }
 
-        public List<string> GetUserInfoCash2(int userId)
+
+
+
+        public List<int> GetUserAccounts(int userId)
+        {
+            List<int> list = new List<int>();
+
+            var accounts = Users.Join(Accounts, u => u.Id, a => a.UserId, (u, a) => new
+            {
+                UserId = a.UserId,
+                OpeningDate = a.OpeningDate,
+                CashAll = a.CashAll,
+                AccountId = a.Id
+            }).Where(u => u.UserId == userId).ToList();
+
+            foreach (var t in accounts)
+            {
+                list.Add(t.AccountId);
+            }
+
+            return list;
+
+        }
+
+
+        public List<string> GetUserInfoCashByAccount(int accountId)
         {
             List<string> list = new List<string>();
 
@@ -59,16 +85,98 @@ namespace Otus.Teaching.Linq.ATM.Core.Services
             {
                 UserId = a.UserId,
                 OpeningDate = a.OpeningDate,
-                CashAll = a.CashAll
-            }).Where(u => u.UserId == userId).ToList();
+                CashAll = a.CashAll,
+                AccountId = a.Id
+            }).Where(a => a.AccountId == accountId).ToList();
 
             foreach (var t in accounts)
             {
-                list.Add(($"Счет открыт {t.OpeningDate}: на счету {t.CashAll} рублей"));
+                list.Add(($"Счет открыт {t.OpeningDate}: на счету {t.CashAll} рублей\n"));
             }
 
             return list;
 
         }
+
+        public List<string> GetUserInfoCashByAccountDetail(int accountId)
+        {
+            List<string> list = new List<string>();
+
+            var hh = History.Where(x => x.AccountId == accountId).ToList();
+            foreach (var t in hh)
+            {
+                list.Add(($"\t{t.OperationDate}: {(t.OperationType == OperationType.InputCash ? "внесено" : "снято")} {t.CashSum} рублей"));
+            }
+
+
+            return list;
+
+        }
+
+        public List<string> GetAllInputCashByUser()
+        {
+            List<string> list = new List<string>();
+
+            var inputCash = (from user in Users
+                             join account in Accounts on user.Id equals account.UserId
+                             join history in History on account.Id equals history.AccountId
+                             select new
+                             {
+                                 user.FirstName,
+                                 user.MiddleName,
+                                 user.SurName,
+                                 history.AccountId,
+                                 history.OperationType,
+                                 history.OperationDate,
+                                 history.CashSum
+
+                             }).Where(x => x.OperationType == OperationType.InputCash).ToList();
+
+
+            foreach (var item in inputCash)
+            {
+                list.Add($"Счет {item.AccountId}  \n\t {item.OperationDate} внесено {item.CashSum} рублей. " +
+                    $"( Счет клиента {item.FirstName} {item.MiddleName} {item.SurName} )");
+            }
+
+
+
+            return list;
+        }
+
+
+        public List<string> GetAllAccountByCashSum(decimal currenSum)
+        {
+            List<string> list = new List<string>();
+
+            var inputCash = (from user in Users
+                             join account in Accounts on user.Id equals account.UserId
+                             //join history in History on account.Id equals history.AccountId
+                             select new
+                             {
+                                 user.FirstName,
+                                 user.MiddleName,
+                                 user.SurName,                               
+                                 account.Id,
+                                 account.CashAll
+
+                             }).Where(x => x.CashAll > currenSum).ToList();
+
+
+            foreach (var item in inputCash)
+            {
+                list.Add($"Счет {item.Id} с общей суммой {item.CashAll} рублей. " +
+                    $"( Счет клиента {item.FirstName} {item.MiddleName} {item.SurName} )");
+            }
+
+
+
+            return list;
+        }
+
+
+
+
+
     }
 }
